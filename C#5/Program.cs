e67b1d511ea0c10de87231d36b115053945ded34
NUREM <NUREMX@GMAIL.COM>
2022-09-29 23:28:46 +0300

    C#5

---
+++ C#5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Name USER :");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "nurem")
+{
+    Console.WriteLine("HI SUPER NUREM");
+}
+else
+{
+    Console.Write("HI :");
+    Console.WriteLine(username);
+}
